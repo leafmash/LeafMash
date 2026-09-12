0815df09fe0ae67a3c1882e32347a3c5942a85b1
leafmash <leafmash@gmail.com>
2026-09-12 09:10:33 +0600

    [skip ci]

---
+++ scripts/ci/inject-microphone-permission.py
@@ -0,0 +1,28 @@
+import re
+import sys
+
+manifest_path = "android/app/src/main/AndroidManifest.xml"
+
+with open(manifest_path, "r") as f:
+    manifest = f.read()
+
+if "android.permission.RECORD_AUDIO" in manifest:
+    print("microphone permission already injected")
+    sys.exit(0)
+
+permission = '    <uses-permission android:name="android.permission.RECORD_AUDIO" />\n'
+
+manifest, count = re.subn(
+    r"(<application[^>]*>)",
+    permission + r"\1",
+    manifest,
+    count=1,
+)
+if count == 0:
+    print("Could not find '<application' tag in AndroidManifest.xml", file=sys.stderr)
+    sys.exit(1)
+
+with open(manifest_path, "w") as f:
+    f.write(manifest)
+
+print("microphone permission injected successfully")
